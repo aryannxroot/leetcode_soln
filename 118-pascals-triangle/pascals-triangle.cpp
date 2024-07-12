a3c9a2cfeac5f969c56f31210dbcf07293de5371
Aryan <ap2303898@gmail.com>
2024-07-12 20:19:34 +0530

    Time: 4 ms (33.24%) | Memory: 8.2 MB (6.82%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -4,18 +4,22 @@ public:
 
     vector<vector<int>>ans;
 
-        for(int i=0;i<numRows;i++){
-            vector<int>temp;
-            for(int j=0;j<=i;j++){
-                if(j==0 || j==i)
-                    temp.push_back(1);
-                else
-                    temp.push_back(ans[i-1][j-1]+ans[i-1][j]);
+    for(int i=0 ; i<numRows ; i++)
+    {
+        vector<int> subans(i+1);
+        for(int j = 0; j<=i ;j++)
+        {
+            if(j==0 || j==i)
+            subans[j] = 1;
+            else
+            {
+                subans[j] = ans[i-1][j-1] + ans[i-1][j];
             }
-            ans.push_back(temp);
         }
-
-        return ans;
+        ans.push_back(subans);
+    }
+        
+    return ans;
 
     }
 };
