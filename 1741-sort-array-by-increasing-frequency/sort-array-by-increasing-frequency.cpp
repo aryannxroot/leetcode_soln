6989fc61de71d1b9245a271561b1a780ee82e8d9
Aryan <ap2303898@gmail.com>
2024-07-23 19:28:40 +0530

    Time: 6 ms (58.80%) | Memory: 15.1 MB (14.29%) - LeetSync

---
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+        unordered_map<int,int> mpp;
+        for(int i : nums) mpp[i]++;
+        sort(nums.begin(),nums.end(), [&](int a,int b){
+            if(mpp[a]==mpp[b]) return a>b;
+            return mpp[a]<mpp[b];
+        });       
+        return nums ;
+    }
+};
