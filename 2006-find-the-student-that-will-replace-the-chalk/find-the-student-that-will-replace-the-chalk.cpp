b84d209f8f768859a62bc1ed87b789d3de4c9893
Aryan <ap2303898@gmail.com>
2024-09-02 12:58:29 +0530

    Time: 98 ms (48.02%) | Memory: 77.1 MB (8.71%) - LeetSync

--- 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/find-the-student-that-will-replace-the-chalk.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
         int sz = chalk.size();
-        long long total_ch = 0;
+        long long total_ch = 0; // sum 
 
         for(int ch : chalk )    
             total_ch += ch;
 
-        k %= total_ch;
+        k %= total_ch; // imp line -> reduce the k ,by removing redundant iterations which goes through whole array 
 
         for(int i = 0; i<sz ;i++)
         {
@@ -15,7 +15,7 @@ public:
             k -= chalk[i];
         }
 
-        return -1;
+        return -1; // dummy return
         
     }
 };
