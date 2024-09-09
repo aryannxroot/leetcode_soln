b54998c0db0ceb08c10dd1a389e83b0bbe2e279b
Aryan <ap2303898@gmail.com>
2024-09-09 15:18:06 +0530

    Time: 170 ms (40.90%) | Memory: 130.5 MB (65.45%) - LeetSync

--- 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
+++ 2411-spiral-matrix-iv/spiral-matrix-iv.cpp
@@ -12,22 +12,46 @@
 class Solution {
 public:
     vector<vector<int>> spiralMatrix(int m, int n, ListNode* head) {
-        vector<vector<int>> ans(m, vector<int>(n, -1));
-       
-        int i = 0, j = 0, cur_d = 0;
-        int direc[4][2] = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
-
-        for (; head != nullptr; head = head->next) {
-            ans[i][j] = head->val;
-
-            int newi = i + direc[cur_d][0], newj = j + direc[cur_d][1];
-   
-            if (min(newi, newj) < 0 || newi >= m || newj >= n || ans[newi][newj] != -1)
-                cur_d = (cur_d + 1) % 4;
-
-            i += direc[cur_d][0];
-            j += direc[cur_d][1];
+        vector< vector<int> > arr(m,vector<int>(n,-1));
+        int minr = 0;
+        int maxr = m-1;
+        int minc = 0;
+        int maxc = n-1;
+        ListNode* temp = head;
+        while(minr<=maxr && minc<=maxc){
+            //rigth
+            for(int j = minc;j<=maxc;j++){
+                if(temp==NULL) return arr;
+                arr[minr][j] = temp->val;
+                temp = temp->next;
+            }
+            minr++;
+            if(minr>maxr || minc>maxc) break;
+            //down
+            for(int i=minr;i<=maxr;i++){
+                if(temp==NULL) return arr;
+                arr[i][maxc] = temp->val;
+                temp = temp->next;
+            }
+            maxc--;
+            if(minr>maxr || minc>maxc) break;
+            //left
+            for(int j=maxc;j>=minc;j--){
+                if(temp==NULL) return arr;
+                arr[maxr][j] = temp->val;
+                temp = temp->next;
+            }
+            maxr--;
+            if(minr>maxr || minc>maxc) break;
+            //top
+            for(int i=maxr;i>=minr;i--){
+                if(temp==NULL) return arr;
+                arr[i][minc] = temp->val;
+                temp = temp->next;
+            }
+            minc++;
+            if(minr>maxr || minc>maxc) break;
         }
-        return ans;
+        return arr;
     }
 };
